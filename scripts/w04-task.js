05e3e7810403112fd540b4a730df92f5e07ba5e3
liviagalletti <liligalletti@gmail.com>
2023-10-07 17:01:59 -0300

    w4 task

---
+++ scripts/w04-task.js
@@ -0,0 +1,55 @@
+let myProfile = {
+    name: "Livia Lira de Medeiros",
+    photo: "images/Me.png",
+    favoriteFoods: [
+        'Sushi',
+        'Lasagna',
+        'Guacamole',
+        'Hamburger',
+        'Chocolate'
+    ],
+    hobbies: [
+        'Playing the piano',
+        'Watching anime',
+        'Walking with my dog'
+    ],
+    placesLived: []
+};
+
+myProfile.placesLived.push(
+    {
+      place: 'Brasília, Brazil',
+      length: '27 years'
+    }
+)
+
+document.querySelector('#name').textContent = myProfile.name;
+document.querySelector('#photo').src = myProfile.photo;
+document.querySelector('#photo').alt = myProfile.name;
+
+const favoriteFoodsList = document.querySelector('#favorite-foods');
+myProfile.favoriteFoods.forEach(food =>{
+    const li = document.createElement('li');
+    li.textContent = food;
+    favoriteFoodsList.appendChild(li);
+});
+
+
+const hobbiesList = document.querySelector('#hobbies');
+myProfile.hobbies.forEach(hobby => {
+  const li = document.createElement('li');
+  li.textContent = hobby;
+  hobbiesList.appendChild(li);
+});
+
+const placesLivedList = document.querySelector('#places-lived');
+myProfile.placesLived.forEach(place => {
+  const dt = document.createElement('dt');
+  dt.textContent = place.place;
+  
+  const dd = document.createElement('dd');
+  dd.textContent = place.length;
+  
+  placesLivedList.appendChild(dt);
+  placesLivedList.appendChild(dd);
+});
