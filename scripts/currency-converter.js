0e166b0930fa4708c1e2321a6304e15696f8c82f
liviagalletti <liligalletti@gmail.com>
2023-10-26 18:33:08 -0300

    currency2

--- scripts/currency-converter.js
+++ scripts/currency-converter.js
@@ -7,50 +7,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const apiKey = "73ae1d3f3e5a47ee84a604655b4d93eb";
 
+    function displayError(message) {
+        console.error(message);
+        resultDiv.textContent = message;
+    }
 
-    fetch(`https://api.currencyfreaks.com/latest?apikey=${apiKey}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok " + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const currencies = data.rates;
-            Object.keys(currencies).forEach(currencyCode => {
-                const option = document.createElement("option");
-                option.value = currencyCode;
-                option.textContent = currencyCode;
-                fromCurrencySelect.appendChild(option);
-                toCurrencySelect.appendChild(option.cloneNode(true));
-            });
-       
-            convertButton.disabled = false;
-        })
-        .catch(error => {
-            console.error("Error fetching the list of currencies:", error);
-            resultDiv.textContent = "Failed to load the list of currencies.";
-        });
+    function fetchCurrencies() {
+        fetch(`https://api.currencyfreaks.com/latest?apikey=${apiKey}`)
+            .then(response => response.ok ? response.json() : Promise.reject("Failed to fetch currencies"))
+            .then(data => {
+                const currencyOptions = Object.keys(data.rates).map(code => {
+                    const option = document.createElement("option");
+                    option.value = code;
+                    option.textContent = code;
+                    return option;
+                });
+                fromCurrencySelect.append(...currencyOptions);
+                toCurrencySelect.append(...currencyOptions.map(option => option.cloneNode(true)));
+                convertButton.disabled = false;
+            })
+            .catch(error => displayError("Failed to load the list of currencies."));
+    }
 
-
-    convertButton.addEventListener("click", () => {
+    function convertCurrency() {
         const fromCurrency = fromCurrencySelect.value;
         const toCurrency = toCurrencySelect.value;
         const amount = parseFloat(amountInput.value);
 
-      
         if (isNaN(amount)) {
-            resultDiv.textContent = "Please enter a valid number.";
-            return;
+            return displayError("Please enter a valid number.");
         }
 
         fetch(`https://api.currencyfreaks.com/latest?apikey=${apiKey}&symbols=${fromCurrency},${toCurrency}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error("Network response was not ok " + response.statusText);
-                }
-                return response.json();
-            })
+            .then(response => response.ok ? response.json() : Promise.reject("Failed to fetch conversion rate"))
             .then(data => {
                 const fromRate = data.rates[fromCurrency];
                 const toRate = data.rates[toCurrency];
@@ -58,9 +47,9 @@ document.addEventListener("DOMContentLoaded", function () {
                 const convertedAmount = (amount * exchangeRate).toFixed(2);
                 resultDiv.textContent = `${amount} ${fromCurrency} = ${convertedAmount} ${toCurrency}`;
             })
-            .catch(error => {
-                console.error("Error fetching the conversion rate:", error);
-                resultDiv.textContent = "Failed to fetch the conversion rate.";
-            });
-    });
+            .catch(error => displayError("Failed to fetch the conversion rate."));
+    }
+
+    fetchCurrencies();
+    convertButton.addEventListener("click", convertCurrency);
 });
